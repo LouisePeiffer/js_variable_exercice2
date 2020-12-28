4d23b616159e4502ef7cbb0b6715cd4669be5b18
Louise <louisepeiffer@hotmail.com>
2020-12-28 14:22:43 +0100

    exo fait

---
+++ public/js/main.js
@@ -0,0 +1,41 @@
+let phrase = "J'aime les tartines et j'en mange"
+console.log(phrase);
+
+let duree = " 4 par jours"
+
+let tout = phrase + duree;
+console.log(tout);
+
+
+let num1 = 5
+console.log(num1);
+let num2 = 35
+console.log(num2);
+let addition = num1 + num2
+console.log(addition);
+
+let lesbails = phrase + num1
+console.log(lesbails);
+
+
+let phrase2 = "Salut tout le monde"
+console.log(phrase2);
+let phrase3 = "Au revoir!"
+console.log(phrase3);
+phrase2 = phrase3
+console.log(phrase2);
+
+let num3 = 4
+let num4 = "6"
+let add1 = num3 > num4
+console.log(add1);
+let add2 = num3 < num4
+console.log(add2);
+let add3 = num3 == num4
+console.log(add3);
+let add4 = num3 =! num4
+console.log(add4);
+let add5 = num3 === num4
+console.log(add5);
+let add6 = num3 !== num4
+console.log(add6);
